441add4ee5926affb9f92fd30d8fb1d169e00fca
xiangzef <414395321@qq.com>
2019-10-17 20:23:11 +0800

    加备注

--- demo1/demo1.py
+++ demo1/demo1.py
@@ -1,6 +1,6 @@
 #排序算法 2-3
 import random
-
+#类 排序数组初始化 函数 1.断言函数用法2.随机数标准写法
 class SortTestHelper:
 
     def __init__(self, n, rangL , rangR):
@@ -9,9 +9,11 @@ class SortTestHelper:
         self.rangR = rangR
 
     def generaterandomarray(self):
+        #1.断言函数用法
         assert self.rangR>self.rangL
         arr = []
         for i in range(self.n):
+            #随机数标准写法
             arr.append(random.randint(1,10000)%(self.rangR-self.rangL+1)+self.rangL)
 
         return  arr
