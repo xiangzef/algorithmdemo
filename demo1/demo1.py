0084ee8c712ff656c65b3f20cbbb38b38bca2856
xiangzef <414395321@qq.com>
2019-10-31 16:27:49 +0800

    修复反向冒泡的一个bug 冒泡是每次都从第一个推到上次的最后一个前一位

--- demo1/demo1.py
+++ demo1/demo1.py
@@ -22,20 +22,27 @@ class SortTestHelper:
             arr.append(random.randint(1,10000)%(self.rangR-self.rangL+1)+self.rangL)
 
         return  arr
-    def selectionsort_bubblesort(self,arr):
+    def selectionsort_bubblesort(self,arr):#冒泡
         for i in range(1,len(arr)):
             for j in range(0,len(arr)-i):#为什么是0 到 n-i 原因是把最大的推到最后 这里是0-18 循环不能超过数组长度
                 if arr[j]>arr[j+1]:
-                    if j ==19 :
-                        j = 19
                     arr[j],arr[j+1] = arr[j+1],arr[j]
         return arr
 
-    def selectionsort_bubblesort_2(self,arr):
+    def selectionsort_bubblesort_2(self,arr):#冒泡2
         for i in range(1,len(arr)):
-            for j in range(i,len(arr)-i):#反推要怎么写 这里的入参2是数组长度 所以是1-19 1，19
-                if arr[len(arr)-j] < arr[len(arr)-1-j]:#最后一个 和前一个比 j = 1就是19 与18比 后一个比前一个小 提到前面来 直到与0比
-                    arr[len(arr) - 1 - j],arr[len(arr)-j] = arr[len(arr)-j],arr[len(arr) - 1 - j]
+            for j in range(i,len(arr)):#反推要怎么写 这里的入参2是数组长度 所以是1-19 1，19
+                if arr[len(arr)-j+i-1] < arr[len(arr)-1-j+i-1]:#最后一个 和前一个比 j = 1就是19 与18比 后一个比前一个小 提到前面来 直到与0比
+                    arr[len(arr)-1-j+i-1],arr[len(arr)-j+i-1] = arr[len(arr)-j+i-1],arr[len(arr)-1-j+i-1]
+        return arr#之前错误的原因是因为  第二次循环没有从最末段开始
+
+    def selectionsort(self, arr):#选择排序
+        for i in range(1,len(arr)):
+            minIndex = i-1
+            for j in range(i,len(arr)):#为什么上面的是 len(arr)-i 下面是len(arr)
+                if arr[j]< arr[minIndex]:
+                    minIndex = j
+            arr[i-1] , arr[minIndex] =arr[minIndex] , arr[i-1]
         return arr
 
 if __name__ == '__main__':
@@ -43,6 +50,6 @@ if __name__ == '__main__':
     #打印
     arr=a.generaterandomarray()
     print(arr)
-    arr=a.selectionsort_bubblesort(arr)
+    arr=a.selectionsort_bubblesort_2(arr)
     print(arr)
     del a ,arr
